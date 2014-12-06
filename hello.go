a40b31acf9a6f69fad1bbf4c53d7c10c2c526cb0
Stefan Schroeder <ondekoza@gmail.com>
2014-12-06 14:35:03 +0100

    Some more stuff

--- hello.go
+++ hello.go
@@ -1,8 +1,41 @@
 package main
 
 import "fmt"
+import "flag"
+
+var name = flag.String("name", "Unknown", "a user name")
+
+var greet = map[string] string {
+	"Stefan": "Hello",
+	"Otto": "Bonjour",
+	"Heinz": "Guten Tag",
+}
 
 func main() {
-	fmt.Println("Good morning, World")
+	defer fmt.Println("Goodbye")
+	defer fmt.Println("Almost done")
+	
+	flag.Parse()
+
+	g, isthere := greet[*name]
+	if ! isthere {
+		g = "Good morning"
+	}
+	fmt.Printf("%s, %s\n", g, *name)
+	µ := 3.141592653589793
+	fmt.Printf("µ is equal %v\n", µ)
+
+	var m string
+	switch (*name) {
+	case "Stefan": m = "Hi Boss"
+	case "Karl": m = "What's up?"
+	case "Fritz": m = "Weeell?"
+	default: m = "Go away"
+	}
+	fmt.Println(m)
+	var x int
+	var y int32
+	var z uint16
+	fmt.Println(x, y, z, x + int(z))
 }
 
